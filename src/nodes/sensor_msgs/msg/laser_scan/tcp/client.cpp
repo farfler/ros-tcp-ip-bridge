38722355f2fce8734478fddebd9851d5f3deaa54
Daniel Bacsur <contact@danielbacsur.com>
2024-03-11 19:50:52 +0100

    feat: add parameter declarations to laser_scan TCP client

--- src/nodes/sensor_msgs/msg/laser_scan/tcp/client.cpp
+++ src/nodes/sensor_msgs/msg/laser_scan/tcp/client.cpp
@@ -8,9 +8,23 @@ class SensorMsgsMsgLaserScanTCPClient : public rclcpp::Node
 public:
     SensorMsgsMsgLaserScanTCPClient() : Node("sensor_msgs_msg_laser_scan_tcp_client")
     {
+        this->declare_parameter<std::string>("ip", "0.0.0.0");
+        this->get_parameter_or<std::string>("ip", ip_, "0.0.0.0");
+        this->declare_parameter<uint16_t>("port", 5000);
+        this->get_parameter_or<uint16_t>("port", port_, 5000);
+
+        this->declare_parameter<std::string>("publisher_topic", "");
+        this->get_parameter_or<std::string>("publisher_topic", publisher_topic_, "");
+        this->declare_parameter<std::string>("subscription_topic", "");
+        this->get_parameter_or<std::string>("subscription_topic", subscription_topic_, "");
     }
 
 private:
+    std::string ip_;
+    uint16_t port_;
+
+    std::string publisher_topic_;
+    std::string subscription_topic_;
 };
 
 int main(int argc, char *argv[])
