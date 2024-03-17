fea52f15ddabe74e8ff254499ebeba91aaeeda5c
Daniel Bacsur <contact@danielbacsur.com>
2024-03-17 01:05:22 +0100

    feat: add geometry_msgs_msg_quaternion message

---
+++ include/tcp_ip_bridge/msgs/geometry_msgs/msg/quaternion.hpp
@@ -0,0 +1,17 @@
+#include <vector> // std::vector
+
+#include "geometry_msgs/msg/quaternion.hpp" // geometry_msgs::msg::Quaternion
+
+namespace tcp_ip_bridge
+{
+
+    class GeometryMsgsMsgQuaternion
+    {
+    public:
+        static std::vector<char> serialize(const std::shared_ptr<geometry_msgs::msg::Quaternion> &msg);
+        static std::vector<char> serialize(const std::shared_ptr<geometry_msgs::msg::Quaternion> &msg, std::vector<char> &packet);
+        static geometry_msgs::msg::Quaternion deserialize(std::vector<char> &packet);
+        static geometry_msgs::msg::Quaternion deserialize(std::vector<char> &packet, geometry_msgs::msg::Quaternion &msg);
+    };
+
+} // namespace tcp_ip_bridge
