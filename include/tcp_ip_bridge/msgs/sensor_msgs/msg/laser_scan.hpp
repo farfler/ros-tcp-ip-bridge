faae3ccafa7f31cd7b55672d19dd51b6bf58805b
Daniel Bacsur <contact@danielbacsur.com>
2024-03-17 12:34:10 +0100

    feat: add sensor_msgs_msg_laser_scan message

---
+++ include/tcp_ip_bridge/msgs/sensor_msgs/msg/laser_scan.hpp
@@ -0,0 +1,17 @@
+#include <vector> // std::vector
+
+#include "sensor_msgs/msg/laser_scan.hpp" // sensor_msgs::msg::LaserScan
+
+namespace tcp_ip_bridge
+{
+
+    class SensorMsgsMsgLaserScan
+    {
+    public:
+        static std::vector<char> serialize(const std::shared_ptr<sensor_msgs::msg::LaserScan> &msg);
+        static std::vector<char> serialize(const std::shared_ptr<sensor_msgs::msg::LaserScan> &msg, std::vector<char> &packet);
+        static sensor_msgs::msg::LaserScan deserialize(std::vector<char> &packet);
+        static sensor_msgs::msg::LaserScan deserialize(std::vector<char> &packet, sensor_msgs::msg::LaserScan &msg);
+    };
+
+} // namespace tcp_ip_bridge
