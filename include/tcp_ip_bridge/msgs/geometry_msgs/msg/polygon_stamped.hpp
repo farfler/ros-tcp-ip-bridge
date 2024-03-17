19f1d7806f9d932adfda7fb9f7918024e0b94ff2
Daniel Bacsur <contact@danielbacsur.com>
2024-03-17 01:01:03 +0100

    feat: add geometry_msgs_msg_polygon_stamped message

---
+++ include/tcp_ip_bridge/msgs/geometry_msgs/msg/polygon_stamped.hpp
@@ -0,0 +1,17 @@
+#include <vector> // std::vector
+
+#include "geometry_msgs/msg/polygon_stamped.hpp" // geometry_msgs::msg::PolygonStamped
+
+namespace tcp_ip_bridge
+{
+
+    class GeometryMsgsMsgPolygonStamped
+    {
+    public:
+        static std::vector<char> serialize(const std::shared_ptr<geometry_msgs::msg::PolygonStamped> &msg);
+        static std::vector<char> serialize(const std::shared_ptr<geometry_msgs::msg::PolygonStamped> &msg, std::vector<char> &packet);
+        static geometry_msgs::msg::PolygonStamped deserialize(std::vector<char> &packet);
+        static geometry_msgs::msg::PolygonStamped deserialize(std::vector<char> &packet, geometry_msgs::msg::PolygonStamped &msg);
+    };
+
+} // namespace tcp_ip_bridge
